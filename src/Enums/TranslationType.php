471d3c8df33beb4658fc7063b8dc002f06ed69e1
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2025-12-29 18:08:54 +0100

    Release v3.0

--- src/Enums/TranslationType.php
+++ src/Enums/TranslationType.php
@@ -19,21 +19,3 @@ enum TranslationType: string
         ];
     }
 }
-
-//
-//class TranslationType
-//{
-//    const Text = 'text';
-//    const Textarea = 'textarea';
-//    const Html = 'html';
-//    const Many = 'many';
-////    const Select = 'select';
-//
-//    const Types = [
-//        self::Text,
-//        self::Textarea,
-//        self::Html,
-//        self::Many,
-////        self::Select,
-//    ];
-//}
